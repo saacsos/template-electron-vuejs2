5fad9d04a38a66430773cae989436e0730cbccf6
Saac <saacsos@gmail.com>
2017-05-17 11:42:52 +0700

    startup vue component

---
+++ app/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <header>
+          <h1 class="ui-header">{{ hello }}</h1>
+          <h2 class="ui-header">{{ hello }}</h2>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                hello: "Hello Electron"
+            }
+        }
+    }
+</script>
